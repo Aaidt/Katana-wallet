285befea1a9f3b4e53c4c1fd9007ba97c8bb8518
Aadit <aadits2004@gmail.com>
2025-06-15 15:17:52 +0000

    sol secret phrases done

--- components/Sol.tsx
+++ components/Sol.tsx
@@ -1,8 +1,11 @@
 import { Keypair } from "@solana/web3.js"
 import nacl from "tweetnacl"
 import { generateMnemonic } from 'bip39'
+import { useState } from "react"
 
 export function Sol() {
+
+    const [generate, setGenerate] = useState(false)
 
     const keypair = Keypair.generate();
     const publicKey = keypair.publicKey.toString();
@@ -39,27 +42,30 @@ export function Sol() {
                     placeholder="Enter your secret recovery phrase (or leave blank to generate)" />
                 <button
                     className="bg-white ml-4 rounded-md duration-200 transition-all px-7 py-3 text-md text-black hover:bg-white/80"
+                    onClick={() => {
+                        setGenerate(true)
+                    }}
                 >
                     Generate Wallet
                 </button>
             </div>
+            {generate && (
 
-            <div className="mt-10 p-6 border border-gray-400 rounded-md ">
-                <h1 className="font-medium text-3xl ">Your secret phrase</h1>
+                <div className="mt-10 p-10 border border-gray-400/30 rounded-md ">
+                    <h1 className="font-medium text-3xl ">Your secret phrase</h1>
 
-                <div className="grid grid-cols-4 grid-rows-3 gap-2 pt-6">
-                    {phrases.map((phrase, index) => {
-                        return (
-                            <div key={index} className="border border-gray-400/20 bg-gray-400/10 text-center
+                    <div className="grid grid-cols-4 grid-rows-3 gap-2 pt-6">
+                        {phrases.map((phrase, index) => {
+                            return (
+                                <div key={index} className="border border-gray-400/20 bg-gray-400/10 text-center
                             text-md font-medium gap-2 rounded-md p-5 cursor-pointer hover:bg-gray-500/30 duration-200 transition-all">
-                                {phrase}
-                            </div>
-                        )
-                    })}
+                                    {phrase}
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-
-            </div>
-
+            )}
         </div>
     );
 }
