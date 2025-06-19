632d4ab99c873f5a2a146207a5518724539f6790
Aadit <aadits2004@gmail.com>
2025-06-19 07:34:49 +0000

    wallet keys done

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -1,5 +1,7 @@
 import { Keypair } from "@solana/web3.js"
+import { Eye, EyeOff } from 'lucide-react'
 import bs58 from 'bs58'
+import { useState } from 'react'
 
 export function Wallet({ n }: { n: number | null }) {
     const keypair = Keypair.generate();
@@ -7,20 +9,42 @@ export function Wallet({ n }: { n: number | null }) {
     const privateKey = keypair.secretKey;
     const privateKeyBase58 = bs58.encode(privateKey)
 
+    const [isVisible, setIsVisible] = useState(false)
+
     if (n === null) return null;
-    return <div className="flex flex-col">
-        <div className="border border-white/30 rounded-2xl p-8 mt-8">
+    return <div className="flex flex-col items-stretch">
+        <div className="border-t border-x border-white/10 rounded-b-none rounded-2xl pt-6 pl-8 pb-12 mt-8 z-10">
             <div className="text-3xl font-medium">
                 Wallet {n}
             </div>
-
         </div>
-        <div className="z-50 bg-white/10 rounded-2xl p-8 mt-5">
-            <div className="text-xl font-medium">Public Key:</div>
+        <div className="z-0 bg-white/10 rounded-2xl p-8 -translate-y-5">
+            <div className="text-xl font-bold">Public Key:</div>
             <div>{publicKey}</div>
             <br />
-            <div className="text-xl font-medium">Private Key:</div>
-            <div>{privateKeyBase58}</div>
+            <div className="text-xl font-bold">Private Key:</div>
+            {isVisible ? (
+                <div className="flex justify-between">
+                    <div>
+                        {privateKeyBase58}
+                    </div>
+                    <EyeOff
+                        onClick={() => {
+                            setIsVisible(false)
+                        }}
+                    />
+                </div>
+            ) : (
+                <div className="flex justify-between">
+                    <div>*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
+                         *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *</div>
+                    <Eye
+                        onClick={() => {
+                            setIsVisible(true)
+                        }}
+                    />
+                </div>
+            )}
         </div>
     </div>
 }
