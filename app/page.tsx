f4cfebfcf4c8c58ba6a833531fa6d40748be568d
Aadit <aadits2004@gmail.com>
2025-09-13 15:18:42 +0530

    removed Ethereum

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,27 @@
 "use client"
 
 import { Swords } from "lucide-react"
-import { useState } from 'react'
 import { Sol } from "@/components/Sol"
-import { Eth } from "@/components/Eth"
-import { Footer } from "@/components/Footer"
 
 export default function Home() {
-    const [sol, setSol] = useState<boolean | null>(null)
-    const [eth, setEth] = useState<boolean | null>(null)
 
-    return (
-        <div>
+   return (
+      <div>
 
-            <div className="flex mx-15 justify-between items-center pt-10">
-                <div
-                    onClick={() => {
-                        setSol(null)
-                        setEth(null)
-                    }}
-                    className="text-3xl font-bold flex items-center gap-2 cursor-pointer">
-                    <Swords
-                        className="w-10 h-10" /> Katana Wallet
-                </div>
+         <div className="flex mx-15 justify-between items-center pt-10">
+            <div className="text-3xl font-bold flex items-center gap-2 cursor-pointer">
+               <Swords
+                  className="w-10 h-10" /> Katana Wallet
             </div>
+         </div>
 
-            <div className="pt-20 pl-15 font-bold text-5xl ">Katana supports multiple blockchains</div>
-            <div className="pl-16 text-gray-400 pt-3 font-thin text-lg">Choose a blockchain to get started with.</div>
+         <div className="pt-20 pl-15 font-bold text-5xl ">Katana supports multiple blockchains</div>
+         <div className="pl-16 text-gray-400 pt-3 font-thin text-lg">Choose a blockchain to get started with.</div>
 
-            <div className="flex pl-16 pt-3 gap-2 ">
-                <button
-                    className="bg-white cursor-pointer rounded-md duration-200 transition-all px-7 py-3 text-md text-black hover:bg-white/80"
-                    onClick={() => setSol(true)}
-                >
-                    Solana
-                </button>
-                <button
-                    className="bg-white rounded-md cursor-pointer duration-200 transition-all px-7 py-3 text-md text-black hover:bg-white/80"
-                    onClick={() => setEth(true)}
-                >
-                    Ethereum
-                </button>
-            </div>
+         <Sol />
 
-            {sol ? (
-                <Sol />
-            ) : eth ? (
-                <Eth />
-            ) : (
-                <div>
-                    <div className="text-gray-400 flex justify-center font-thin text-lg p-16 pt-20">
-                        Start by selecting one blockchain.
-                    </div>
-                </div>
-            )}
+      </div>
 
 
-            <Footer />
-
-
-        </div>
-
-
-    )
+   )
 }
